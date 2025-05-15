4dadacc844d6228928895c59842414cd328e32ca
André Sóstenes Ámérico <andresosteness@gmail.com>
2025-05-15 16:56:58 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja_ebac/login.cy.js
@@ -0,0 +1,12 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login',() => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('andre.teste@teste.com')
+        cy.get('#password').type('andré1234')
+        cy.get('.woocommerce-form > .button').click()
+        
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, andre.teste (não é andre.teste? Sair)')
+    })
+})
