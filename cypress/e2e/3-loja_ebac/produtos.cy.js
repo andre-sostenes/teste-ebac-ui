38c8dcea12474deac0aed4e0bd214c94196a67b2
André Sóstenes Ámérico <andresosteness@gmail.com>
2025-05-19 18:12:38 -0300

    seleção de produtos

---
+++ cypress/e2e/3-loja_ebac/produtos.cy.js
@@ -0,0 +1,11 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    }); 
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row').contains('Apollo Running Short').click()
+        cy.get('.product_title').should('contain', 'Apollo Running Short')
+    });
+});
